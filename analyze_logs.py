bcecdc94a4729004a2217ce21cee59214dcd40b4
Simon Priadka <manik949@gmail.com>
2020-05-05 11:30:23 +0200

    Initial commit

---
+++ analyze_logs.py
@@ -0,0 +1,8 @@
+from pm4py.algo.discovery.alpha import factory as alpha_miner
+from pm4py.objects.log.importer.xes import factory as xes_importer
+from pm4py.visualization.petrinet import factory as vis_factory
+
+log = xes_importer.import_log('/Users/spriadka/Downloads/Chapter_1/running-example.xes')
+net, initial_marking, final_marking = alpha_miner.apply(log)
+gviz = vis_factory.apply(net, initial_marking, final_marking)
+vis_factory.view(gviz)
---
+++ gather_jira.py
@@ -0,0 +1,90 @@
+from jira import JIRA
+
+import codecs
+import os
+import sys
+
+def write_xes(basename, dir, issues):
+
+    import xes
+
+    output = codecs.open(os.path.join(dir, basename + '-process.xes'), 'w', encoding='utf-8')
+    log = xes.Log()
+    log.global_event_attributes = [
+        xes.Attribute(type = "string", key = "concept:name", value = "__INVALID__"),
+        xes.Attribute(type = "string", key = "lifecycle:transition", value = "complete")
+    ]
+    log.global_trace_attributes = [
+        xes.Attribute(type = "string", key = "concept:name", value = "__INVALID__")
+    ]
+    for issue in issues:
+        trace = xes.Trace()
+        trace.add_attribute(xes.Attribute(type = "string", key = "concept:name", value = issue["key"]))
+        createdEvent = xes.Event()
+        createdEvent.add_attribute(xes.Attribute(type = "string", key="org:resource", value = issue["author"]))
+        createdEvent.add_attribute(xes.Attribute(type = "date", key = "time:timestamp", value = issue["created"]))
+        createdEvent.add_attribute(xes.Attribute(type = "string", key = "concept:name", value = "Open"))
+        trace.add_event(createdEvent)
+        for transition in issue['transitions']:
+            event = xes.Event()
+            event.add_attribute(xes.Attribute(type = "string", key="org:resource", value = transition["who"]))
+            event.add_attribute(xes.Attribute(type = "date", key = "time:timestamp", value = transition["when"]))
+            if (transition["what"]["field"] == "status"):
+                eventValue = transition["what"]["toString"]
+            elif (transition["what"]["field"] == "description"):
+                eventValue = "Change Description"
+            else:
+                eventValue = "Change Field: {field}".format(field = transition["what"]["field"])
+            event.add_attribute(xes.Attribute(type = "string", key = "concept:name", value = eventValue))
+            trace.add_event(event)
+        log.add_trace(trace)
+    log.classifiers = [
+        xes.Classifier(name="MXML Legacy Classifier", keys="concept:name lifecycle:transition"),
+        xes.Classifier(name="Event Name", keys="concept:name"),
+        xes.Classifier(name="Resource", keys="org:resource")
+    ]
+    output.write(str(log))
+
+def transformTransitionAuthor(transition):
+    try:
+        return transition.author.displayName
+    except AttributeError:
+        return ""
+def transformIssueAuthor(issue):
+    try:
+        return issue.fields.creator.displayName
+    except AttributeError:
+        return ""
+
+def getTransitions(issue):
+    transitions = []
+    for history in issue.changelog.histories:
+        for item in history.items:
+            transition = {
+                'when' : history.created,
+                'who' : transformTransitionAuthor(history),
+                'what' : {
+                    'field' : item.field,
+                    'fromString' : item.fromString if item.fromString else "Undefined",
+                    'toString' : item.toString if item.toString else "Undefined"
+                }
+            }
+            transitions.append(transition)
+    return transitions
+
+jira = JIRA(server="https://issues.redhat.com")
+
+issues = jira.search_issues('project=WFLY AND type="Feature Request"', maxResults=-1, expand="changelog")
+
+transformedIssues = []
+
+for issue in issues:
+    transformedIssue = {
+        'key' : issue.key,
+        'created' : issue.fields.created,
+        'author' : transformIssueAuthor(issue),
+        'transitions' : getTransitions(issue)
+    }
+    transformedIssues.append(transformedIssue)
+
+write_xes('wildfly-feature-requests', '.', transformedIssues)
