22dd987e2cc8de60f7dcac45108aa8cd67c1ca90
Michal Cervenansky <mcervena@redhat.com>
2020-06-21 15:44:42 +0200

    Travis works and args test works

--- test_CI.py
+++ test_CI.py
@@ -10,6 +10,13 @@ def test_answer():
 
 
 def test_args():
+    # 256 seems to be ValueError
     assert 256 == os.system('python jira-pm/__main__.py')
     assert 256 == os.system('python jira-pm/__main__.py arg_one')
     assert 256 == os.system('python jira-pm/__main__.py arg_one arg_two')
+
+
+def test_output():
+    os.system(
+        """python jira-pm/__main__.py https://issues.redhat.com 'project=WFLY AND type="Feature Request"' wildfly-feature-requests""")
+    
