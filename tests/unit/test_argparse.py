548c4de9be36de12055d23cf970593c8317938f4
Simon Priadka <manik949@gmail.com>
2020-06-21 17:21:24 +0200

    Fixed tests

--- tests/unit/test_argparse.py
+++ tests/unit/test_argparse.py
@@ -12,10 +12,10 @@ def test_answer():
 def test_args():
     # 256 seems to be ValueError
     assert 256 == os.system('python jira-pm/__main__.py')
-    assert 256 == os.system('python jira-pm/__main__.py arg_one')
-    assert 256 == os.system('python jira-pm/__main__.py arg_one arg_two')
+    assert 512 == os.system('python jira-pm/__main__.py arg_one')
+    assert 512 == os.system('python jira-pm/__main__.py arg_one arg_two')
 
 
 def test_output():
     os.system(
-        """python jira-pm/__main__.py https://issues.redhat.com 'project=WFLY AND type="Feature Request"' wildfly-feature-requests""")
+        """python jira-pm/__main__.py --jira-server https://issues.redhat.com --query 'project=WFLY AND type="Feature Request"' wildfly-feature-requests""")
